5f9ce7821b87d6d6f34dbda0161cc24c42d71ef4
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-06-18 19:16:33 +0200

    :sparkles: Add SSO to Login Component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Popup } from '../../sharedComponents/popup/popup';
 
 const Login = () => {
-  return <div>Please Login</div>;
+  useEffect(() => {
+    popupLogin();
+  }, []);
+  return <div></div>;
 };
+
+const popupLogin = () => {
+  Popup(process.env.REACT_APP_SSO_DOMAIN + '/login', 'Login', 800, 400);
+  var eventMethod = window.addEventListener
+    ? 'addEventListener'
+    : 'attachEvent';
+  var eventer = window[eventMethod];
+  var messageEvent = eventMethod === 'attachEvent' ? 'onmessage' : 'message';
+
+  eventer(
+    messageEvent,
+    function (e) {
+      if (e.origin !== process.env.REACT_APP_SSO_DOMAIN) {
+        return;
+      }
+      localStorage.setItem('token', e.data.token);
+      window.location.replace('/home');
+    },
+    false
+  );
+};
+
 export default Login;
