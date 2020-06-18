3cf13a63fa94de4f1a732a1abb226330cdc9f84b
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-06-18 16:44:01 +0200

    🔧 Setup ANTD configuration files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './App.less';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div className="App">TEST</div>;
+};
 
 export default App;
