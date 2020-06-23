971f04e4554d16e08e159720816971cbb29a1659
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-06-24 01:33:29 +0200

    :sparkles: Add New URLS

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -3,4 +3,6 @@ export const urls = {
   course: '/api/v1/academy/course',
   lesson: '/api/v1/academy/lesson',
   me: '/api/v1/academy/me',
+  episode: '/api/v1/academy/episode',
+  triggerPoll: '/api/v1/academy/episode/pollevent',
 };
